f84efddae428bffaf6c52d4c246027612eeed933
dtanigaw <dtanigaw@student.42.fr>
2024-05-03 20:59:12 +0200

    Grace: OK

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #define FNAME "Grace_kid.c"
-#define S "#include <stdio.h>%c#define FNAME %cGrace_kid.c%c%c#define S %c%s%c%c%cint	main() {%c	FILE	*f = fopen(FNAME, %cw%c);%c	if (!f) return 1;%c	fprintf(f, S, 10, 34, 34, 10, 34, S, 34, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10);%c	return 0;%c}%c"
-
-int	main() {
-	FILE	*f = fopen(FNAME, "w");
-	if (!f) return 1;
-	fprintf(f, S, 10, 34, 34, 10, 34, S, 34, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10);
-	return 0;
-}
+#define S "#include <stdio.h>%c#define FNAME %cGrace_kid.c%c%c#define S %c%s%c%c#define MAIN() int main() {FILE *f = fopen(FNAME, %cw%c);if (!f) return 1;fprintf(f, S, 10, 34, 34, 10, 34, S, 34, 10, 34, 34, 10, 10, 10);return 0;}%c// Run quine%cMAIN();%c"
+#define MAIN() int main() {FILE *f = fopen(FNAME, "w");if (!f) return 1;fprintf(f, S, 10, 34, 34, 10, 34, S, 34, 10, 34, 34, 10, 10, 10);return 0;}
+// Run quine
+MAIN();
--- Makefile
+++ Makefile
@@ -3,16 +3,22 @@ QUINE2	= Grace
 
 .PHONY: all clean fclean re
 
-all: $(QUINE1)
+all: $(QUINE1) $(QUINE2)
 
 $(QUINE1):
 	$(MAKE) -C $(QUINE1)
 
+$(QUINE2):
+	$(MAKE) -C $(QUINE2)
+
 clean:
 	$(MAKE) -C $(QUINE1) clean
+	$(MAKE) -C $(QUINE2) clean
 	
 fclean:
 	$(MAKE) -C $(QUINE1) fclean
+	$(MAKE) -C $(QUINE2) fclean
 
 re:
 	$(MAKE) -C $(QUINE1) re
+	$(MAKE) -C $(QUINE2) re
