8928ac187f57b3d363410359cfdc6fedae21a6cd
dtanigaw <dtanigaw@student.42.fr>
2024-05-08 22:34:22 +0200

    Simplify (todo: remove nl)

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define FNAME "Grace_kid.c"
-#define S "#include <stdio.h>%c#define FNAME %cGrace_kid.c%c%c#define S %c%s%c%c#define MAIN() int main() {FILE *f = fopen(FNAME, %cw%c);if (!f) return 1;fprintf(f, S, 10, 34, 34, 10, 34, S, 34, 10, 34, 34, 10, 10, 10);fclose(f);return 0;}%c// Run quine%cMAIN();%c"
-#define MAIN() int main() {FILE *f = fopen(FNAME, "w");if (!f) return 1;fprintf(f, S, 10, 34, 34, 10, 34, S, 34, 10, 34, 34, 10, 10, 10);fclose(f);return 0;}
+#define S "#include <stdio.h>%1$c#define FNAME %2$cGrace_kid.c%2$c%1$c#define S %2$c%3$s%2$c%1$c#define MAIN() int main() {FILE *f = fopen(FNAME, %2$cw%2$c);if (!f) return 1;fprintf(f, S, 10, 34, S);fclose(f);return 0;}%1$c// Run quine%1$cMAIN();"
+#define MAIN() int main() {FILE *f = fopen(FNAME, "w");if (!f) return 1;fprintf(f, S, 10, 34, S);fclose(f);return 0;}
 // Run quine
 MAIN();
