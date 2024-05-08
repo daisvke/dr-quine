b0b25d31860c971cd473ffd1ba052bf5aa35662e
dtanigaw <dtanigaw@student.42.fr>
2024-05-09 01:33:40 +0200

    simplify

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #define FNAME "Sully"
 #define COUNTER 5
-#define S "#include <stdio.h>%c#include <stdlib.h>%c#define FNAME %cSully%c#define COUNTER 5%c#define S %c%s%c%c#define MAIN() int main() {char *filename[100]; char *cmd[100];if (COUNTER < 1) return 0;sprintf(filename, %c%cs_%cd.c%c, FNAME, --COUNTER);FILE *f = fopen(FNAME, %cw%c);if (!f) return 1;sprintf(cmd, %cgcc %cs_%cd.c -o %cs%c, FNAME, COUNTER, filename);fprintf(f, S, 10, 10, 34, 34, 10, 34, S, 34, 10, 34, 37, 37, 34, 34, 34);fclose(f);return 0;}%c// Run quine%cMAIN();"
-#define MAIN() int main() {char filename[100];char cmd[100];if (COUNTER < 1) return 0;sprintf(filename, "%s_%d.c", FNAME, --COUNTER); FILE *f = fopen(filename, "w");if (!f) return 1; sprintf(cmd, "gcc %s_%d.c -o %s", FNAME, COUNTER, filename);fprintf(f, S, 10, 10, 34, 34, 10, 34, S, 34, 10, 34, 37, 37, 34, 34, 34, 34, 37, 37, 37, 34 10, 10);fclose(f);return 0;}
+#define FNAME_FMT "%s_%d.c"
+#define GCC "gcc %s_%d.c -o %s"
+#define S "#include <stdio.h>%1$c#include <stdlib.h>%1$c#define FNAME %2$cSully%2$c#define COUNTER 5%1$c#define S %2$c%3$s%2$c%1$c#define MAIN() int main() {char *filename[100]; char *cmd[100];if (COUNTER < 1) return 0;sprintf(filename, %2$c%4$cs_%4$cd.c%2$c, FNAME, --COUNTER);FILE *f = fopen(FNAME, %2$cw%2$c);if (!f) return 1;sprintf(cmd, %2$cgcc %4$cs_%4$cd.c -o %4$cs%2$c, FNAME, COUNTER, filename);fprintf(f, S, 10, 34, S, 37);fclose(f);return 0;}%1$c// Run quine%1$cMAIN();"
+#define MAIN() int main() {char filename[100];char cmd[100];if (COUNTER < 1) return 0;sprintf(filename, FNAME_FMT, FNAME, --COUNTER); FILE *f = fopen(filename, "w");if (!f) return 1; sprintf(cmd, GCC, FNAME, COUNTER, filename);fprintf(f, S, 10, 34, S, 37);fclose(f);return 0;}
 // Run quine
-int main(){MAIN();}
+MAIN();
