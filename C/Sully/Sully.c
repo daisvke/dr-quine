6959377157609e2d367518a0138627584219b099
dtanigaw <dtanigaw@student.42.fr>
2024-05-08 19:01:43 +0200

    before 1st try Sully

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#define FNAME "Grace_kid.c"
-#define S "#include <stdio.h>%c#define FNAME %cGrace_kid.c%c%c#define S %c%s%c%c#define MAIN() int main() {FILE *f = fopen(FNAME, %cw%c);if (!f) return 1;fprintf(f, S, 10, 34, 34, 10, 34, S, 34, 10, 34, 34, 10, 10, 10);return 0;}%c// Run quine%cMAIN();%c"
-#define MAIN() int main() {FILE *f = fopen(FNAME, "w");if (!f) return 1;fprintf(f, S, 10, 34, 34, 10, 34, S, 34, 10, 34, 34, 10, 10, 10);return 0;}
+#include <stdlib.h>
+#define FNAME "Sully"
+#define COUNTER 5
+#define S "#include <stdio.h>%c#include <stdlib.h>%c#define FNAME %cSully%c#define COUNTER 5%c#define S %c%s%c%c#define MAIN() int main() {char *filename[100]; char *cmd[100]; if (COUNTER < 1) return 0;sprintf(filename, %c%cs_%cd.c%c, FNAME, --COUNTER);FILE *f = fopen(FNAME, %cw%c);if (!f) return 1;fprintf(f, S, 10, 10, 34, 34, 10, 34, S, 34, 10, 34, 37, 37, 34, 34, 34, 10, 10, 10);return 0;}%c// Run quine%cMAIN();%c"
+#define MAIN() int main() {char *filename[100]; char *cmd[100]; if (COUNTER < 1) return 0;sprintf(filename, "%s_%d.c", FNAME, --COUNTER); FILE *f = fopen(filename, "w");if (!f) return 1; sprintf(cmd, "gcc %s_%d.c -o %s", FNAME, COUNTER, filename);fprintf(f, S, 10, 34, 34, 10, 34, S, 34, 10, 34, 34, 10, 10, 10, FNAME);return 0;}
 // Run quine
 MAIN();
