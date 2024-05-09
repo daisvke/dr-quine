c863193fc0511b2546b194ff4b8e153cc17f090d
daisvke <daisuketanigawa@live.fr>
2024-05-09 03:13:47 +0200

    C: OK

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-// This is a quine
+/*
+	This is a quine
+*/
 char	*gets() {
-	return "#include <stdio.h>%2$c%2$c// This is a quine%2$cchar	*gets() {%2$c	return %3$c%1$s%3$c;%2$c}%2$c%2$cint main() {%2$c	// The output will be the same as the code%2$c	char *s = gets();%2$c	printf(s, s, 10, 34);%2$c	return 0;%2$c}";
+	return "#include <stdio.h>%2$c%2$c/*%2$c	This is a quine%2$c*/%2$cchar	*gets() {%2$c	return %3$c%1$s%3$c;%2$c}%2$c%2$cint main() {%2$c	/*%2$c		The output will be the same as the code%2$c	*/%2$c	char *s = gets();%2$c	printf(s, s, 10, 34);%2$c	return 0;%2$c}";
 }
 
 int main() {
-	// The output will be the same as the code
+	/*
+		The output will be the same as the code
+	*/
 	char *s = gets();
 	printf(s, s, 10, 34);
 	return 0;
--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #define FNAME "Grace_kid.c"
-#define S "#include <stdio.h>%1$c#define FNAME %2$cGrace_kid.c%2$c%1$c#define S %2$c%3$s%2$c%1$c#define MAIN() int main() {FILE *f = fopen(FNAME, %2$cw%2$c);if (!f) return 1;fprintf(f, S, 10, 34, S);fclose(f);return 0;}%1$c// Run quine%1$cMAIN();"
-#define MAIN() int main() {FILE *f = fopen(FNAME, "w");if (!f) return 1;fprintf(f, S, 10, 34, S);fclose(f);return 0;}
-// Run quine
-MAIN();
+#define S "#include <stdio.h>%1$c#define FNAME %2$cGrace_kid.c%2$c%1$c#define S %2$c%3$s%2$c%1$c#define FT(S, FNAME) int main() {FILE *f = fopen(FNAME, %2$cw%2$c);if (!f) return 1;fprintf(f, S, 10, 34, S);fclose(f);return 0;}%1$c%1$c/*%1$c	Run quine%1$c*/%1$cFT(S, FNAME);"
+#define FT(S, FNAME) int main() {FILE *f = fopen(FNAME, "w");if (!f) return 1;fprintf(f, S, 10, 34, S);fclose(f);return 0;}
+
+/*
+	Run quine
+*/
+FT(S, FNAME);
