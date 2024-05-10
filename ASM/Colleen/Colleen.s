1785e2825643d829e4e8d4148a49e5939cc9dc1f
daisvke <daisuketanigawa@live.fr>
2024-05-10 22:24:01 +0200

    Different exiting method

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,11 +1,11 @@
 ; This is a quine
 
 section .rodata
-	fmt: db "; This is a quine%1$c%1$csection .rodata%1$c	fmt: db %2$c%3$s%2$c, 0x0%1$c%1$csection .text%1$cextern printf%1$cextern exit%1$cglobal main%1$c%1$cget_quine:%1$c	lea rdi, [rel fmt]%1$c	mov rsi, 0xa%1$c	mov rdx, 0x22%1$c	lea rcx, [rel fmt]%1$c	xor al, al%1$c	call printf%1$c	xor rax, rax%1$c	leave%1$c	ret%1$c%1$cmain:%1$c	; Prologue%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	call get_quine%1$c	ret", 0x0
+	fmt: db "; This is a quine%1$c%1$csection .rodata%1$c	fmt: db %2$c%3$s%2$c, 0x0%1$c%1$csection .text%1$cextern printf%1$cextern exit%1$cglobal main%1$c%1$cget_quine:%1$c	lea rdi, [rel fmt]%1$c	mov rsi, 0xa%1$c	mov rdx, 0x22%1$c	lea rcx, [rel fmt]%1$c	xor al, al%1$c	call printf%1$c	xor rax, rax%1$c	leave%1$c	call exit%1$c%1$cmain:%1$c	; Prologue%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	call get_quine%1$c	ret", 0x0
 
 section .text
 extern printf
-extern  exit
+extern exit
 global main
 
 get_quine:
@@ -15,9 +15,7 @@ get_quine:
 	lea rcx, [rel fmt]
 	xor al, al
 	call printf
-	xor rax, rax
-	leave
-	call exit
+	ret
 
 main:
 	; Prologue
@@ -25,4 +23,5 @@ main:
 	mov rbp, rsp
 
 	call get_quine
-	ret
+	leave
+	call exit
