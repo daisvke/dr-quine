7ca1fa26353a9c3a3fa6b1d1e3971176902ccb4b
dtanigaw <dtanigaw@student.42.fr>
2024-05-12 23:10:44 +0200

    Works on PC2

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,11 +1,10 @@
 ; This is a quine
 
 section .rodata
-	fmt: db "; This is a quine%1$c%1$csection .rodata%1$c	fmt: db %2$c%3$s%2$c, 0x0%1$c%1$csection .text%1$cextern printf%1$cextern exit%1$cglobal main%1$c%1$cget_quine:%1$c	lea rdi, [rel fmt]%1$c	mov rsi, 0xa%1$c	mov rdx, 0x22%1$c	lea rcx, [rel fmt]%1$c	xor al, al%1$c	call printf%1$c	ret%1$c%1$cmain:%1$c	; Prologue%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	call get_quine%1$c	leave%1$c	xor edi, edi%1$c	call exit", 0x0
+	fmt: db "; This is a quine%1$c%1$csection .rodata%1$c	fmt: db %2$c%3$s%2$c, 0x0%1$c%1$csection .text%1$cextern printf%1$cglobal main%1$c%1$cget_quine:%1$c	lea rdi, [rel fmt]%1$c	mov rsi, 0xa%1$c	mov rdx, 0x22%1$c	lea rcx, [rel fmt]%1$c	xor al, al%1$c	call printf%1$c	ret%1$c%1$cmain:%1$c	; Prologue%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	call get_quine%1$c	leave%1$c	mov rax, 60%1$c	xor edi, edi%1$c	syscall", 0x0
 
 section .text
 extern printf
-extern exit
 global main
 
 get_quine:
--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -1,18 +1,14 @@
 ; This is a quine
 
 %define FNAME "Grace_kid.s"
-%define FMT "; This is a quine%1$c%1$c%2$cdefine FNAME %3$cGrace_kid.s%3$c%1$c%2$cdefine FMT %3$c%4$s%3$c%1$c%1$csection .rodata%1$c	filename: db FNAME, 0x0%1$c	fmt: db FMT, 0x0%1$c	mode: db %3$cw%3$c, 0x0%1$c%1$csection .text%1$c	extern fprintf%1$c	extern fopen%1$c	extern fclose%1$c	extern exit%1$c	global main%1$c%1$c%2$cmacro MAIN 0x0%1$cmain:%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	lea rdi, [rel filename]%1$c	lea rsi, [rel mode]%1$c	call fopen%1$c	push rax%1$c%1$c	test rax, rax%1$c	js _exit%1$c%1$c	mov rdi, rax%1$c	lea rsi, [rel fmt]%1$c	mov rdx, 0xa%1$c	mov rcx, 0x25%1$c	mov r8, 0x22%1$c	lea r9, [rel fmt]%1$c	xor al, al%1$c	call fprintf%1$c%1$c	pop rdi%1$c	call fclose%1$c%1$c	leave%1$c	xor edi, edi%1$c	call exit%1$c%1$c_exit:%1$c	leave%1$c	mov edi, 0x1%1$c	call exit%1$c%2$cendmacro%1$c%1$cMAIN"
+%define FMT "; This is a quine%1$c%1$c%2$cdefine FNAME %3$cGrace_kid.s%3$c%1$c%2$cdefine FMT %3$c%4$s%3$c%1$c%1$csection .rodata%1$c	filename: db FNAME, 0x0%1$c	fmt: db FMT, 0x0%1$c%1$csection .text%1$c	extern dprintf%1$c	global _start%1$c%1$c%2$cmacro MAIN 0x0%1$cmain:%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	lea rdi, [rel filename]%1$c	mov rsi, 0101%1$c	mov rdx, 0666o%1$c	mov rax, 2%1$c	syscall%1$c	push rax%1$c%1$c	test rax, rax%1$c	js _exit%1$c%1$c	mov rdi, rax%1$c	lea rsi, [rel fmt]%1$c	mov rdx, 0xa%1$c	mov rcx, 0x25%1$c	mov r8, 0x22%1$c	lea r9, [rel fmt]%1$c	xor al, al%1$c	call dprintf%1$c%1$c	pop rdi%1$c	mov rax, 3%1$c	syscall%1$c%1$c	leave%1$c	xor edi, edi%1$c	mov rax, 60%1$c	syscall%1$c%1$c_exit:%1$c	leave%1$c	mov edi, 0x1%1$c	mov rax, 60%1$c	syscall%1$c%2$cendmacro%1$c%1$c_start:%1$c	MAIN"
 
 section .rodata
 	filename: db FNAME, 0x0
 	fmt: db FMT, 0x0
-	mode: db "w", 0x0
 
 section .text
 	extern dprintf
-	extern fopen
-	extern fclose
-	extern exit
 	global _start
 
 %macro MAIN 0x0
@@ -22,7 +18,7 @@ main:
 
 	lea rdi, [rel filename]
 	mov rsi, 0101
-    mov rdx, 0666o
+	mov rdx, 0666o
 	mov rax, 2
 	syscall
 	push rax
@@ -44,14 +40,15 @@ main:
 	syscall
 
 	leave
-	mov rax, 60
 	xor edi, edi
+	mov rax, 60
 	syscall
 
 _exit:
 	leave
 	mov edi, 0x1
-	call exit wrt ..plt
+	mov rax, 60
+	syscall
 %endmacro
 
 _start:
