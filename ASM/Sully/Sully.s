8b52008131ed083bf6aff05f7f9e54c3ee70b7bd
dtanigaw <dtanigaw@student.42.fr>
2024-05-26 13:16:42 +0200

    Works on PC2

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,10 +1,11 @@
 section .data
 	i: dq 5
-	buffer: times 100 db 0
+	fd: dq 0
+	buffer: times 100 db 0x0
 	filename: db "Sully_%1$d.s", 0x0
-	content: db "section .data%1$c	i: dq 5%1$c	buffer: times 100 db 0x0%1$c	filename: db %2$cSully_%1$c%d.s%2$c, 0x0%1$c	content: db %2$c%4$s%2$c, 0x0%1$c	compile: db %2$cnasm -f elf64 Sully_%1$c && ld --dynamic-linker=/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc Sully_%1$c.o -o Sully_%1$c%2$c, 0x0%1$c	exec: db %2$cSully_%1$c%2$c, 0x0%1$c%1$csection .text%1$c	extern dprintf%1$c	extern sprintf%1$c	extern system%1$c	global _start%1$c%1$c%2$cmacro SPRINT_FILENAME 0x3%1$c	mov rdi, %1%1$c	mov rsi, %2%1$c	mov rdx, %3%1$c	call sprintf%1$c%2$c%2$cmacro EXIT 0x1%1$c	leave%1$c	mov edi, %1%1$c	mov rax, 60%1$c	syscall%1$c%2$c%2$cmacro OPEN_FILE 0x1%1$c	mov rdi, %1%1$c	mov rsi, 0101%1$c	mov rdx, 0666o%1$c	mov rax, 0x2%1$c	syscall%1$c%2$c%2$cmacro FPRINT_FILECONTENT 0x1%1$c	mov rdi, rax%1$c	lea rsi, %1%1$c	mov rdx, 0xa%1$c	mov rcx, 0x25%1$c	mov r8, 0x22%1$c	mov r9, %1%1$c	xor al, al%1$c	call dprintf%1$c%2$c%2$cmacro MAIN 0x0%1$cmain:%1$c	SPRINT_FILENAME buffer, filename, i%1$c	OPEN_FILE buffer%1$c	test rax, rax%1$c	js EXIT 0x1%1$c	FPRINT_FILECONTENT content%1$c	sub rcx, 1%1$c	mov rax, 3%1$c	syscall%1$c	SPRINT_FILENAME buffer, compile, i%1$c	mov rdi, buffer%1$c	call system%1$c	cmp qword [i], 0%1$c	jle EXIT 0x0%1$c	SPRINT_FILENAME buffer, exec, i%1$c	mov rdi, buffer%1$c	call system%1$c%2$c_start:%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	MAIN%1$c	EXIT 0x0%1$c", 0x0
-	compile: db "nasm -f elf64 Sully_%1$d && ld --dynamic-linker=/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc Sully_%1$d.o -o Sully_%1$d", 0x0
-	exec: db "Sully_%1$d", 0x0
+	content: db "section .data%1$c	i: dq %2$d%1$c	fd: dq 0%1$c	buffer: times 100 db 0x0%1$c	filename: db %3$cSully_%%1$d.s%3$c, 0x0%1$c	content: db %3$c%4$s%3$c, 0x0%1$c	compile: db %3$cnasm -felf64 Sully_%%1$d.s && ld --dynamic-linker=/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc Sully_%%1$d.o -o Sully_%%1$d%3$c, 0x0%1$c	exec: db %3$c./Sully_%%1$d%3$c, 0x0%1$c%1$csection .text%1$c	extern dprintf%1$c	extern sprintf%1$c	extern system%1$c	global _start%1$c%1$c%%macro SPRINT_FILENAME 0x3%1$c	mov rdi, %%1%1$c	mov rsi, %%2%1$c	mov rdx, qword %%3%1$c	call sprintf%1$c%%endmacro%1$c%1$c%%macro OPEN_FILE 0x1%1$c	mov rdi, %%1%1$c	mov esi, 01102o%1$c	mov edx, 0666o%1$c	mov eax, 0x2%1$c	syscall%1$c%%endmacro%1$c%1$c%%macro FPRINT_FILECONTENT 0x1%1$c	mov rdi, qword [fd]%1$c	mov rsi, %%1%1$c	mov edx, 0xa%1$c	mov rcx, qword [i]%1$c	sub rcx, 0x1%1$c	mov r8d, 0x22%1$c	mov r9, %%1%1$c	call dprintf%1$c%%endmacro%1$c%1$c%%macro MAIN 0x0%1$cmain:%1$c	SPRINT_FILENAME buffer, filename, [i]%1$c	OPEN_FILE buffer%1$c	test rax, rax%1$c	js return%1$c%1$c	mov [fd], rax%1$c	FPRINT_FILECONTENT content%1$c%1$c	mov rdi, qword [fd]%1$c	mov eax, 0x3%1$c	syscall%1$c%1$c	SPRINT_FILENAME buffer, compile, [i]%1$c	mov rdi, buffer%1$c	call system%1$c%1$c	cmp qword [i], 0x0%1$c	jle return%1$c%1$c	SPRINT_FILENAME buffer, exec, [i]%1$c	mov rdi, buffer%1$c	call system%1$c%%endmacro%1$c%1$c_start:%1$c	MAIN%1$c%1$creturn:%1$c	mov eax, 0x3c%1$c	xor edi, edi%1$c	syscall", 0x0
+	compile: db "nasm -felf64 Sully_%1$d.s && ld --dynamic-linker=/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc Sully_%1$d.o -o Sully_%1$d", 0x0
+	exec: db "./Sully_%1$d", 0x0
 
 section .text
 	extern dprintf
@@ -15,54 +16,49 @@ section .text
 %macro SPRINT_FILENAME 0x3
 	mov rdi, %1
 	mov rsi, %2
-	mov rdx, %3
+	mov rdx, qword %3
 	call sprintf
 %endmacro
 
-%macro EXIT 0x1
-	leave
-	mov edi, %1
-	mov rax, 60
-	syscall
-%endmacro
-
-%macro OPEN_FILE 0x0
-	mov rsi, 0101
-	mov rdx, 0666o
-	mov rax, 0x2
+%macro OPEN_FILE 0x1
+	mov rdi, %1
+	mov esi, 01102o
+	mov edx, 0666o
+	mov eax, 0x2
 	syscall
 %endmacro
 
 %macro FPRINT_FILECONTENT 0x1
-	mov rdi, rax
+	mov rdi, qword [fd]
 	mov rsi, %1
-	mov rdx, 0xa
-	mov rcx, 0x22
-	mov r8, 0x25
+	mov edx, 0xa
+	mov rcx, qword [i]
+	sub rcx, 0x1
+	mov r8d, 0x22
 	mov r9, %1
-	xor al, al
 	call dprintf
 %endmacro
 
 %macro MAIN 0x0
 main:
 	SPRINT_FILENAME buffer, filename, [i]
-	OPEN_FILE
+	OPEN_FILE buffer
 	test rax, rax
-	js EXIT 0x1
+	js return
 
+	mov [fd], rax
 	FPRINT_FILECONTENT content
-	sub rcx, 1
 
-	mov rax, 3
+	mov rdi, qword [fd]
+	mov eax, 0x3
 	syscall
 
 	SPRINT_FILENAME buffer, compile, [i]
 	mov rdi, buffer
 	call system
 
-	cmp qword [i], 0
-	jle EXIT 0x0
+	cmp qword [i], 0x0
+	jle return
 
 	SPRINT_FILENAME buffer, exec, [i]
 	mov rdi, buffer
@@ -70,8 +66,9 @@ main:
 %endmacro
 
 _start:
-	push rbp
-	mov rbp, rsp
-
 	MAIN
-	EXIT 0x0
+
+return:
+	mov eax, 0x3c
+	xor edi, edi
+	syscall
