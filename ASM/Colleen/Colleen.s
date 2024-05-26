357d304a76cbb3be9d802a336c81a7a148157915
dtanigaw <dtanigaw@student.42.fr>
2024-05-26 17:10:15 +0200

    Works on PC2, del prologue+epilogue

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,28 +1,20 @@
 ; This is a quine
 
 section .rodata
-	fmt: db "; This is a quine%1$c%1$csection .rodata%1$c	fmt: db %2$c%3$s%2$c, 0x0%1$c%1$csection .text%1$cextern printf%1$cglobal main%1$c%1$cget_quine:%1$c	lea rdi, [rel fmt]%1$c	mov esi, 0xa%1$c	mov edx, 0x22%1$c	lea rcx, [rel fmt]%1$c	xor al, al%1$c	call printf%1$c	ret%1$c%1$cmain:%1$c	; Prologue%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	call get_quine%1$c	leave%1$c	mov eax, 0x3c%1$c	xor edi, edi%1$c	syscall", 0x0
+	fmt: db "; This is a quine%1$c%1$csection .rodata%1$c	fmt: db %2$c%3$s%2$c, 0x0%1$c%1$csection .text%1$cextern printf%1$cglobal main%1$c%1$cmain:%1$c	lea rdi, [rel fmt]%1$c	mov esi, 0xa%1$c	mov edx, 0x22%1$c	lea rcx, [rel fmt]%1$c	xor al, al%1$c	call printf%1$c%1$c	mov eax, 0x3c%1$c	xor edi, edi%1$c	syscall", 0x0
 
 section .text
 extern printf
 global main
 
-get_quine:
+main:
 	lea rdi, [rel fmt]
 	mov esi, 0xa
 	mov edx, 0x22
 	lea rcx, [rel fmt]
 	xor al, al
 	call printf
-	ret
 
-main:
-	; Prologue
-	push rbp
-	mov rbp, rsp
-
-	call get_quine
-	leave
 	mov eax, 0x3c
 	xor edi, edi
 	syscall
