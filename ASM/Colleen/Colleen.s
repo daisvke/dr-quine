d863c2d2aa7c44ce5d6c98b53ac622cab7335fba
dtanigaw <dtanigaw@student.42.fr>
2024-06-15 16:57:06 +0200

    extern exit

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -2,7 +2,7 @@ bits 64
 ; This is a quine
 
 section .rodata
-	fmt: db "bits 64%1$c; This is a quine%1$c%1$csection .rodata%1$c	fmt: db %2$c%3$s%2$c, 0x0%1$c%1$csection .text%1$cextern printf, exit%1$cglobal main%1$c%1$cprintstr:%1$c	; The output will be the same as the code%1$c	push rbp%1$c	lea rdi, [rel fmt]%1$c	mov esi, 0xa%1$c	mov edx, 0x22%1$c	lea rcx, [rel fmt]%1$c	xor al, al%1$c	call printf%1$c	pop rbp%1$c	ret%1$c%1$cmain:%1$c	call printstr%1$c	mov rdi, 0x0%1$c	call exit", 0x0
+	fmt: db "bits 64%1$c; This is a quine%1$c%1$csection .rodata%1$c	fmt: db %2$c%3$s%2$c, 0x0%1$c%1$csection .text%1$cextern printf, exit%1$cglobal main%1$c%1$cprintstr:%1$c	; The output will be the same as the code%1$c	push rbp%1$c	lea rdi, [rel fmt]%1$c	mov esi, 0xa%1$c	mov edx, 0x22%1$c	lea rcx, [rel fmt]%1$c	xor al, al%1$c	call printf%1$c	pop rbp%1$c	ret%1$c%1$cmain:%1$c	call printstr%1$c	xor rdi, rdi%1$c	call exit", 0x0
 
 section .text
 extern printf, exit
@@ -22,5 +22,5 @@ printstr:
 
 main:
 	call printstr
-	mov rdi, 0x0
+	xor rdi, rdi
 	call exit
--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -1,7 +1,8 @@
+bits 64
 ; This is a quine
 
 %define FNAME "Grace_kid.s"
-%define FMT "; This is a quine%1$c%1$c%2$cdefine FNAME %3$cGrace_kid.s%3$c%1$c%2$cdefine FMT %3$c%4$s%3$c%1$c%1$csection .data%1$c	filename: db FNAME, 0x0%1$c	fmt: db FMT, 0x0%1$c	fd: dd 0%1$c%1$csection .text%1$c	extern dprintf%1$c	global _start%1$c%1$c%2$cmacro OPEN 0x0%1$c	lea rdi, [rel filename]%1$c	mov esi, 01102o%1$c	mov edx, 0666o%1$c	mov rax, 2%1$c	syscall%1$c%2$cendmacro%1$c%1$c%2$cmacro DPRINTF 0x1%1$c	mov rdi, %2$c1%1$c	lea rsi, [rel fmt]%1$c	mov edx, 0xa%1$c	mov ecx, 0x25%1$c	mov r8d, 0x22%1$c	lea r9, [rel fmt]%1$c	call dprintf%1$c%2$cendmacro%1$c%1$c%2$cmacro MAIN 0x0%1$cmain:%1$c	OPEN%1$c%1$c	test rax, rax%1$c	js return%1$c	mov [fd], rax%1$c%1$c	DPRINTF rax%1$c%1$c	mov rdi, [fd]%1$c	mov eax, 3%1$c	syscall%1$c%1$creturn:%1$c	xor edi, edi%1$c	mov eax, 0x3c%1$c	syscall%1$c%2$cendmacro%1$c%1$c_start:%1$c	MAIN"
+%define FMT "bits 64%1$c; This is a quine%1$c%1$c%2$cdefine FNAME %3$cGrace_kid.s%3$c%1$c%2$cdefine FMT %3$c%4$s%3$c%1$c%1$csection .data%1$c	filename: db FNAME, 0x0%1$c	fmt: db FMT, 0x0%1$c	fd: dd 0%1$c%1$csection .text%1$c	extern dprintf, exit%1$c	global _start%1$c%1$c%2$cmacro OPEN 0x0%1$c	lea rdi, [rel filename]%1$c	mov esi, 01102o%1$c	mov edx, 0666o%1$c	mov rax, 2%1$c	syscall%1$c%2$cendmacro%1$c%1$c%2$cmacro DPRINTF 0x1%1$c	mov rdi, %2$c1%1$c	lea rsi, [rel fmt]%1$c	mov edx, 0xa%1$c	mov ecx, 0x25%1$c	mov r8d, 0x22%1$c	lea r9, [rel fmt]%1$c	call dprintf%1$c%2$cendmacro%1$c%1$c%2$cmacro MAIN 0x0%1$cmain:%1$c	OPEN%1$c%1$c	test rax, rax%1$c	js return%1$c	mov [fd], rax%1$c%1$c	DPRINTF rax%1$c%1$c	mov rdi, [fd]%1$c	mov eax, 3%1$c	syscall%1$c%1$ccall exit%1$c%2$cendmacro%1$c%1$c_start:%1$c	MAIN"
 
 section .data
 	filename: db FNAME, 0x0
@@ -9,7 +10,7 @@ section .data
 	fd: dd 0
 
 section .text
-	extern dprintf
+	extern dprintf, exit
 	global _start
 
 %macro OPEN 0x0
@@ -44,10 +45,7 @@ main:
 	mov eax, 3
 	syscall
 
-return:
-	xor edi, edi
-	mov eax, 0x3c
-	syscall
+	call exit
 %endmacro
 
 _start:
